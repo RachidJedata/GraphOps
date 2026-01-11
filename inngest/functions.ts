52bc1d1f88bf2601c533abc1b2b3d9e199899bc5
RachidJedata <jedatarachid@gmail.com>
2026-01-11 17:11:11 +0100

    feat: implement AI execution function and update dependencies

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,10 +1,28 @@
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from '@ai-sdk/google'
+import { createOpenAI } from '@ai-sdk/openai'
+import { createAnthropic } from '@ai-sdk/anthropic'
+import { generateText } from 'ai'
 
-export const helloWorld = inngest.createFunction(
-    { id: "hello-world" },
-    { event: "test/hello.world" },
+const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+const anthropic = createAnthropic();
+
+export const executeAI = inngest.createFunction(
+    { id: "execute-ai" },
+    { event: "execute/ai" },
     async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "1s");
-        return { message: `Hello ${event.data.email}!` };
+
+        const { steps } = await step.ai.wrap(
+            "gemini-generate-text",
+            generateText,
+            {
+                model: "google/gemini-2.5-flash",
+                system: "",
+                prompt: "",
+            }
+        )
+
+        return steps;
     },
 );
